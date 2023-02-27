cf2b32424f1e75cbbb72a3baf95c3a200f7826d0
your_username <pad128g@icloud.com>
2023-02-27 18:12:41 -0500

    finish

--- src/ui-components/NoteUpdateForm.d.ts
+++ src/ui-components/NoteUpdateForm.d.ts
@@ -4,38 +4,44 @@
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
 
-import * as React from "react";
-import { GridProps, TextFieldProps } from "@aws-amplify/ui-react";
-import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
-import { Note } from "../models";
+import * as React from 'react'
+import { GridProps, TextFieldProps } from '@aws-amplify/ui-react'
+import { EscapeHatchProps } from '@aws-amplify/ui-react/internal'
+import { Note } from '../models'
+
 export declare type ValidationResponse = {
-    hasError: boolean;
-    errorMessage?: string;
-};
-export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
+  hasError: boolean
+  errorMessage?: string
+}
+export declare type ValidationFunction<T> = (
+  value: T,
+  validationResponse: ValidationResponse
+) => ValidationResponse | Promise<ValidationResponse>
 export declare type NoteUpdateFormInputValues = {
-    name?: string;
-    description?: string;
-};
+  name?: string
+  description?: string
+}
 export declare type NoteUpdateFormValidationValues = {
-    name?: ValidationFunction<string>;
-    description?: ValidationFunction<string>;
-};
-export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+  name?: ValidationFunction<string>
+  description?: ValidationFunction<string>
+}
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>
 export declare type NoteUpdateFormOverridesProps = {
-    NoteUpdateFormGrid?: PrimitiveOverrideProps<GridProps>;
-    name?: PrimitiveOverrideProps<TextFieldProps>;
-    description?: PrimitiveOverrideProps<TextFieldProps>;
-} & EscapeHatchProps;
-export declare type NoteUpdateFormProps = React.PropsWithChildren<{
-    overrides?: NoteUpdateFormOverridesProps | undefined | null;
-} & {
-    id?: string;
-    note?: Note;
-    onSubmit?: (fields: NoteUpdateFormInputValues) => NoteUpdateFormInputValues;
-    onSuccess?: (fields: NoteUpdateFormInputValues) => void;
-    onError?: (fields: NoteUpdateFormInputValues, errorMessage: string) => void;
-    onChange?: (fields: NoteUpdateFormInputValues) => NoteUpdateFormInputValues;
-    onValidate?: NoteUpdateFormValidationValues;
-} & React.CSSProperties>;
-export default function NoteUpdateForm(props: NoteUpdateFormProps): React.ReactElement;
+  NoteUpdateFormGrid?: PrimitiveOverrideProps<GridProps>
+  name?: PrimitiveOverrideProps<TextFieldProps>
+  description?: PrimitiveOverrideProps<TextFieldProps>
+} & EscapeHatchProps
+export declare type NoteUpdateFormProps = React.PropsWithChildren<
+  {
+    overrides?: NoteUpdateFormOverridesProps | undefined | null
+  } & {
+    id?: string
+    note?: Note
+    onSubmit?: (fields: NoteUpdateFormInputValues) => NoteUpdateFormInputValues
+    onSuccess?: (fields: NoteUpdateFormInputValues) => void
+    onError?: (fields: NoteUpdateFormInputValues, errorMessage: string) => void
+    onChange?: (fields: NoteUpdateFormInputValues) => NoteUpdateFormInputValues
+    onValidate?: NoteUpdateFormValidationValues
+  } & React.CSSProperties
+>
+export default function NoteUpdateForm(props: NoteUpdateFormProps): React.ReactElement
